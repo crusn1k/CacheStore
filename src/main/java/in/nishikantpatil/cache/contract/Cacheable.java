cb8f9fbf917e3bd2f80576b405c76bcea34bcb18
Nishikant Ajay Patil <crusnik02.trinityblood@gmail.com>
2017-04-22 00:35:02 +0530

    refactor

--- src/main/java/in/nishikantpatil/cache/contract/Cacheable.java
+++ src/main/java/in/nishikantpatil/cache/contract/Cacheable.java
@@ -2,6 +2,6 @@ package in.nishikantpatil.cache.contract;
 
 import java.io.Serializable;
 
-public interface Cacheable<Key> extends Serializable {
+public interface Cacheable<Key extends Serializable> {
     Key getKey();
 }
