c5b162807571474fa46a3cfcd4aca25faf005689
Nishikant Ajay Patil <crusnik02.trinityblood@gmail.com>
2017-04-22 00:58:05 +0530

    refactor

--- src/main/java/in/nishikantpatil/cache/CacheStore.java
+++ src/main/java/in/nishikantpatil/cache/CacheStore.java
@@ -8,34 +8,33 @@ import java.util.concurrent.ConcurrentMap;
 
 public final class CacheStore {
     private static final ConcurrentMap<Class<? extends Cacheable>, ConcurrentMap<Serializable, Cacheable<? extends Serializable>>> store = new ConcurrentHashMap<>();
-    private static final ConcurrentMap<Class<? extends Cacheable<Serializable>>, Long> sequenceMap = new ConcurrentHashMap<>();
+    private static final ConcurrentMap<Class<? extends Cacheable<? extends Serializable>>, Long> sequenceMap = new ConcurrentHashMap<>();
 
-    public static <T extends Cacheable<Serializable>> long getNextSequenceNumber(Class<T> clazz) {
+    public static long getNextSequenceNumber(Class<? extends Cacheable<? extends Serializable>> clazz) {
         sequenceMap.putIfAbsent(clazz, Long.MIN_VALUE);
         sequenceMap.put(clazz, sequenceMap.get(clazz) + 1);
         return sequenceMap.get(clazz);
     }
 
-    public static <T extends Cacheable<? extends Serializable>> T put(T item) {
+    public static Cacheable<? extends Serializable> put(Cacheable<? extends Serializable> item) {
         if (null == item) {
             throw new IllegalArgumentException("Item cannot be null!");
         }
         store.putIfAbsent(item.getClass(), new ConcurrentHashMap<>());
-        store.get(item.getClass()).put(item.getKey(), item);
-        return item;
+        return store.get(item.getClass()).put(item.getKey(), item);
     }
 
-    public static <T extends Cacheable<? extends Serializable>> T get(Serializable key, Class<T> clazz) {
+    public static Cacheable<? extends Serializable> get(Serializable key, Class<? extends Cacheable<? extends Serializable>> clazz) {
         if (store.containsKey(clazz) && store.get(clazz).containsKey(key)) {
-            return (T) store.get(clazz).get(key);
+            return store.get(clazz).get(key);
         }
         return null;
     }
 
     public static void dump() {
-        store.entrySet().forEach(storeEntry -> {
-            System.out.printf("---------------------------%s---------------------------\n", storeEntry.getKey());
-            storeEntry.getValue().values().forEach(System.out::println);
+        store.forEach((key, value) -> {
+            System.out.printf("---------------------------%s---------------------------\n", key);
+            value.values().forEach(System.out::println);
         });
     }
 }
